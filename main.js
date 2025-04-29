d92b7efb7177da77597acd1f488857eb44787fa0
SukharamD <sukharama@gmail.com>
2025-04-30 00:14:23 +0530

    Responsive Home background

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ var typed = new Typed('.text', {
     backDelay:100,
     loop:true
   });
+
+let bar=document.getElementById("bar")
+let nav=document.getElementById("navbar")
+let close=document.getElementById("close")
+
+if(bar)
+{
+    bar.addEventListener("click",()=>{
+        nav.classList.toggle("active")
+    })
+}
+
+if(close){
+    close.addEventListener("click",()=>{
+        nav.classList.remove("active")
+    })
+}
